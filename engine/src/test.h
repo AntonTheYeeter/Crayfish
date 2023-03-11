c6a1bd6740c15757af6fc59c72c18cfc378d8eb1
AntonTheYeeter <antonlindner2007@gmail.com>
2023-03-11 18:38:01 +0100

    setup

---
+++ engine/src/test.h
@@ -0,0 +1,8 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include "def.h"
+
+CFAPI void test(const char* str);
+
+#endif
---
+++ testapp/src/testapp.c
@@ -0,0 +1,8 @@
+#include <test.h>
+
+int main(void)
+{
+    test("This is a test!");
+
+    return 0;
+}
