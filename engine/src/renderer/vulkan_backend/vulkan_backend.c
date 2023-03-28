96d305fabc9546e0d8f55283d29bfaab93be6d11
Anton Dev <72763857+AntonTheYeeter@users.noreply.github.com>
2023-03-28 18:46:59 +0200

    vulkan device

--- engine/src/renderer/vulkan_backend/vulkan_backend.c
+++ engine/src/renderer/vulkan_backend/vulkan_backend.c
@@ -100,6 +100,115 @@ b8 vulkan_rendererBackendStartup(PlatformWindow* win, u32 windowWidth, u32 windo
         context.surface = createSurface(win, context.instance, context.allocator);
     }
 
+    // GPU
+    {
+        u32 gpuCount = 0;
+        VK_CHECK(vkEnumeratePhysicalDevices(context.instance, &gpuCount, PNULL));
+        VkPhysicalDevice gpus[gpuCount];
+        VK_CHECK(vkEnumeratePhysicalDevices(context.instance, &gpuCount, gpus));
+
+        b8 graphicsQueueFound = FALSE;
+        b8 presentQueueFound = FALSE;
+        b8 transferQueueFound = FALSE;
+
+        for(u32 i = 0; i < gpuCount; i++)
+        {
+            VkPhysicalDeviceProperties props;
+            vkGetPhysicalDeviceProperties(gpus[i], &props);
+
+            u32 queueFamilyCount = 0;
+            vkGetPhysicalDeviceQueueFamilyProperties(gpus[i], &queueFamilyCount, PNULL);
+            VkQueueFamilyProperties queueFamilies[queueFamilyCount];
+            vkGetPhysicalDeviceQueueFamilyProperties(gpus[i], &queueFamilyCount, queueFamilies);
+
+            if(props.deviceType != VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+            {
+                CF_WARN("This device doesn't have a discrete GPU which may lead to bugs!");
+            }
+
+            for(u32 j = 0; j < queueFamilyCount; j++)
+            {
+                VkBool32 presentSupport = VK_FALSE;
+                VK_CHECK(vkGetPhysicalDeviceSurfaceSupportKHR(gpus[i], j, context.surface, &presentSupport));
+
+                if(queueFamilies[j].queueFlags & VK_QUEUE_GRAPHICS_BIT && !graphicsQueueFound)
+                {
+                    context.graphicsQueueIndex = j;
+                    graphicsQueueFound = TRUE;
+                }
+                else if(presentSupport && !presentQueueFound)
+                {
+                    context.presentQueueIndex = j;
+                    presentQueueFound = TRUE;
+                }
+                else if(queueFamilies[j].queueFlags & VK_QUEUE_TRANSFER_BIT && !transferQueueFound)
+                {
+                    context.transferQueueIndex = j;
+                    transferQueueFound = TRUE;
+                }
+            }
+
+            if(graphicsQueueFound && presentQueueFound && transferQueueFound)
+            {
+                CF_DEBUG("Selected GPU %s", props.deviceName);
+                CF_DEBUG("Graphics Queue Index: %i Present Queue Index: %i Transfer Queue Index: %i", context.graphicsQueueIndex, context.presentQueueIndex, context.transferQueueIndex);
+                context.gpu = gpus[i];
+                break;
+            }
+        }
+
+        if(!graphicsQueueFound || !presentQueueFound || !transferQueueFound)
+        {
+            CF_FATAL("No suitable GPU found!");
+            return FALSE;
+        }
+    }
+
+    // Device
+    {
+        f32 queuePriority = 1.0f;
+
+        VkDeviceQueueCreateInfo graphicsQueueInfo = {VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO};
+        graphicsQueueInfo.queueFamilyIndex = context.graphicsQueueIndex;
+        graphicsQueueInfo.queueCount = 1;
+        graphicsQueueInfo.pQueuePriorities = &queuePriority;
+
+        VkDeviceQueueCreateInfo presentQueueInfo = {VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO};
+        presentQueueInfo.queueFamilyIndex = context.presentQueueIndex;
+        presentQueueInfo.queueCount = 1;
+        presentQueueInfo.pQueuePriorities = &queuePriority;
+
+        VkDeviceQueueCreateInfo transferQueueInfo = {VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO};
+        transferQueueInfo.queueFamilyIndex = context.transferQueueIndex;
+        transferQueueInfo.queueCount = 1;
+        transferQueueInfo.pQueuePriorities = &queuePriority;
+
+        VkDeviceQueueCreateInfo queueInfos[] = {graphicsQueueInfo, presentQueueInfo, transferQueueInfo};
+
+        VkDeviceCreateInfo deviceInfo = {VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO};
+        deviceInfo.queueCreateInfoCount = ARRAY_SIZE(queueInfos);
+        deviceInfo.pQueueCreateInfos = queueInfos;
+
+        const char* deviceExtensions[] =
+        {
+            VK_KHR_SWAPCHAIN_EXTENSION_NAME
+        };
+
+        deviceInfo.enabledExtensionCount = ARRAY_SIZE(deviceExtensions);
+        deviceInfo.ppEnabledExtensionNames = deviceExtensions;
+
+        VkPhysicalDeviceFeatures features;
+        vkGetPhysicalDeviceFeatures(context.gpu, &features);
+
+        deviceInfo.pEnabledFeatures = &features;
+
+        VK_CHECK(vkCreateDevice(context.gpu, &deviceInfo, context.allocator, &context.device));
+
+        vkGetDeviceQueue(context.device, context.graphicsQueueIndex, 0, &context.graphicsQueue);
+        vkGetDeviceQueue(context.device, context.presentQueueIndex, 0, &context.presentQueue);
+        vkGetDeviceQueue(context.device, context.transferQueueIndex, 0, &context.transferQueue);
+    }
+
     return TRUE;
 }
 
